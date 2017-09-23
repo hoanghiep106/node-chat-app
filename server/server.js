dac81d5665a24818fa75292b39f7a6aa72076cf3
Hoang Hiep Nguyen <hoanghiepnguyen106@gmail.com>
2017-09-23 12:13:41 +0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,12 +15,27 @@ app.use(express.static('public'));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'New user joined'
+    });
+
     socket.on('createMessage', (newMessage) => {
         io.emit('newMessage', {
-            from: newMessage.to,
+            from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
         });
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
